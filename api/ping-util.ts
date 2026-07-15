3929f4023e6ebbd451c04dfa9dd5bb7741fb27df
Hicham <daghour200@gmail.com>
2026-07-15 21:32:48 +0100

    Probe: capture util import error

--- api/ping-util.ts
+++ api/ping-util.ts
@@ -1,6 +1,11 @@
-import { slug } from "./_lib/util";
-export default function handler(_req: any, res: any) {
-  res.statusCode = 200;
+export default async function handler(_req: any, res: any) {
   res.setHeader("content-type", "application/json");
-  res.end(JSON.stringify({ ok: true, slug: slug("Hello World") }));
+  try {
+    const m: any = await import("./_lib/util");
+    res.statusCode = 200;
+    res.end(JSON.stringify({ ok: true, slug: m.slug("Hello World") }));
+  } catch (e: any) {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ err: String((e && e.stack) || e) }));
+  }
 }
